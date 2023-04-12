95d72c10fc2e49ecc9d36b48447e10c2fb5eeb64
khoitq-acl <khoitq@americancodelab.com>
2023-04-13 00:31:45 +0700

    Update

--- backend/start.sh
+++ backend/start.sh
@@ -6,5 +6,4 @@ git pull
 PID=`ps -ef | grep k-playlist/backend | awk '{ print $2 }'`
 kill -9 $PID
 
-cd k_playlist_backend/
-python3 manage.py runserver 0.0.0.0:6000 &
+cd k_playlist_backend/ && python3 manage.py runserver 0.0.0.0:6000 &
